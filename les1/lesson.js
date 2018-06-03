23f2920c700d77123f85923c8e0b4739df3bbf9b
Maksim <luzamo83@gmail.com>
2018-06-03 23:54:28 +0600

    Добавление функций

--- les1/lesson.js
+++ les1/lesson.js
@@ -1,16 +1,52 @@
-var budget = prompt("Ваш бюджет на месяц?");
-var name = prompt("Название вашего магазина?");
+var budget,
+	name,
+	time,
+	price = 100
+
+function start () {
+	budget = prompt("Ваш бюджет на месяц?");
+	while (isNaN(budget) || budget == '' || budget == null) {
+		start(budget);
+	}
+	name = prompt("Название вашего магазина?").toUpperCase();
+	console.log(name);
+}
+
+start();
+
 var mainList = {
 	budget,
 	name,
 	shopGoods: [],
 	emploeyers: {},
+	discount: true,
 	open: true
 };
-
+function chooseGoods() { 
 for (let i = 0; i < 3; i++) {
 	mainList.shopGoods[i] = prompt('Какой тип товара будем продавать?');
 }
+}
+chooseGoods();
+function dayBudget() {
+ 	document.write("Дневной бюджет составляет " + budget/30 + " р.");
+ } 
+ dayBudget();
+ price = 100;
+ function disc() {
+ 	if (mainList.discount == true) {
+ 		console.log(price * 0.8);
+ 	} 
+ 	
+ }
+ disc();
+function emplName () {
+for (let i = 1; i <= 4; i++) {
+ 	mainList.emploeyers[i] = prompt('Имя сотрудника');
+ 	 }
+ 	}
+ emplName();
+ 	 console.log(mainList.emploeyers);
 /*let i = 0;
 while (i < 3) {
 	mainList.shopGoods[i] = prompt ('Какой тип товара будем продавать?'); i++;
@@ -24,4 +60,3 @@ while (i < 3) {
 /*mainList.shopGoods[0] = prompt('Какой тип товара будем продавать?');
 mainList.shopGoods[1] = prompt('Какой тип товара будем продавать?');
 mainList.shopGoods[2] = prompt('Какой тип товара будем продавать?');*/
-document.write("Дневной бюджет составляет " + budget/30 + " р.");
