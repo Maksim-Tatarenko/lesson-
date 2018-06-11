7af09e486694735ecdc0f5864b0d73b640f3ef35
Maksim <luzamo83@gmail.com>
2018-06-11 22:59:16 +0600

    Подготовка к 4-му заданию

--- les1/lesson.js
+++ les1/lesson.js
@@ -1,15 +1,17 @@
 var budget,
 	name,
 	time,
-	price = 100
+	price = 100;
 
-	function start () {
-		budget = prompt("Ваш бюджет на месяц?");
-			while (isNaN(budget) || budget == '' || budget == null) {
-				start(budget);
-		}
-		name = prompt("Название вашего магазина?").toUpperCase();
-			console.log(name);
+function start () {
+	budget = prompt("Ваш бюджет на месяц?");
+
+	while (isNaN(budget) || budget == '' || budget == null) {
+		start(budget);
+	}
+
+	name = prompt("Название вашего магазина?").toUpperCase();
+	console.log(name);
 }
 
 start();
@@ -20,67 +22,98 @@ var mainList = {
 	shopGoods: [],
 	emploeyers: {},
 	discount: true,
-	open: true
-};
-	function chooseGoods() { 
+	open: true,
+	time: 23,
+	shopItems: [],
+	chooseGoods: function chooseGoods() { 
 		for (let i = 1; i < 4; i++) {
-		let a = prompt('Какой тип товара будем продавать?');
-		
-		if ((typeof(a) === 'string' && (typeof(a)) != null && a != " " && a.length < 50)) {
-			console.log("Все правильно!");
-			mainList.shopGoods[i] = a;
-		} else {
-		i--;
-}
-}
-}
-chooseGoods();
-
-
-	
-	function dayBudget() {
-	 	document.write("Дневной бюджет составляет " + budget/30 + " р.");
- } 
- 
- dayBudget();
- 
- price = 100;
-
-	 function disc() {
-	 	if (mainList.discount == true) {
-	 		console.log(price * 0.8);
- 	} 
- 	
- }
- disc();
-
-	function emplName () {
+			let a = prompt('Какой тип товара будем продавать?');
+			
+			if ((typeof(a) === 'string' && a != null && a != "" && a.length < 50)) {
+				console.log("Все правильно!");
+				mainList.shopGoods[i] = a;
+			} else {
+				i--;
+			}
+		}	
+	},
+	workTime: function workTime(time) {
+			if (time < 0) {
+				console.log('Такого не может быть.');	
+			} 	else if (time > 8 && time < 20) {
+					console.log('Время работать!');
+					mainList.open = true;
+				}	else if (time > 24) {
+						console.log('Уже слишком поздно!');
+					}	else {
+							console.log('В сутках только 24 часа!')
+						}
+	},
+	dayBudget: function dayBudget() {
+		document.write("Дневной бюджет составляет " + budget/30 + " р.");
+	}, 
+	disc: function disc() {
+		if (mainList.discount == true) {
+			console.log(price * 0.8);
+		}	else {
+				console.log(price);
+			}
+	},
+	emplName: function emplName () {
 		for (let i = 1; i <= 4; i++) {
-	 	mainList.emploeyers[i] = prompt('Имя сотрудника');
- 	 }
- 	}
- emplName();
- 	console.log(mainList.emploeyers);
+			mainList.emploeyers[i] = prompt('Имя сотрудника');
+		}
+	},
+	chooseShopItems: function chooseShopItems () {
+		let items = prompt('Перечислите все товары через запятую', "");
+			if (typeof(items) === 'string' && items != null && items != '') {		
+				mainList.shopItems = items.split(",");
+				mainList.shopItems.push(prompt('Подумайте еще', ""));
+				mainList.shopItems.sort(); 
+			}	else {
+					items = prompt('Введите правельные данные.');
+				}
+	}
+}
 
- mainList.shopGoods.forEach(function(item,i,arr) {
+mainList.chooseGoods();
+	console.log(mainList.shopGoods);
+mainList.workTime();
+mainList.dayBudget();
+mainList.disc();
+mainList.emplName();
+	console.log(mainList.emploeyers);
+mainList.chooseShopItems();
+	console.log(mainList.shopItems);
+
+
+
+
+/*mainList.shopGoods.forEach(function(item,i,arr) {
 	alert('У нас вы можете преобрести: ' + i + ': ' + arr[i]);
 })
 
- 		for (var prop in mainList) {
- 			console.log("Наш магазин содержит: " + (prop + ': ' + mainList[prop]));
- 		}
-
- 	 //console.log(mainList.emploeyers);
-/*let i = 0;
+for (var prop in mainList) {
+	console.log("Наш магазин содержит: " + (prop + ': ' + mainList[prop]));
+}*/
+ 
+//console.log(mainList.emploeyers);
+/*
+let i = 0;
 while (i < 3) {
 	mainList.shopGoods[i] = prompt ('Какой тип товара будем продавать?'); i++;
-	}*/
-/*let i = 0;
-	do {
-		mainList.shopGoods[i] = prompt('Какой тип товара будем продавать?'); i++;
-	}
-	while( i < 3);*/
+}
+*/
+/*
+let i = 0;
+do {
+	mainList.shopGoods[i] = prompt('Какой тип товара будем продавать?'); i++;
+}
+while( i < 3);
+*/
 	
-/*mainList.shopGoods[0] = prompt('Какой тип товара будем продавать?');
+/*
+mainList.shopGoods[0] = prompt('Какой тип товара будем продавать?');
 mainList.shopGoods[1] = prompt('Какой тип товара будем продавать?');
-mainList.shopGoods[2] = prompt('Какой тип товара будем продавать?');*/
+mainList.shopGoods[2] = prompt('Какой тип товара будем продавать?');
+*/
